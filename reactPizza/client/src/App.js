a6cffa4c510fad3fc78451f0318dd696eec70925
khabibjon Kuchkarboev <habib6796@icloud.com>
2022-11-18 12:18:38 +0300

    Add files via upload

---
+++ reactPizza/client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+import "./scss/app.scss";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Додо_пиццы" prace={500}/>
+            
+            <PizzaBlock  title="Мексиканская"/>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ reactPizza/client/src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li className="">Мясные</li>
+        <li className="">Вегетарианская</li>
+        <li className="">Гриль</li>
+        <li className="">Острые</li>
+        <li className="">Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
