fdc140368b3b1ab029bb93bcf6ffcc48869f9798
khabibjon Kuchkarboev <97886241+khxabib@users.noreply.github.com>
2022-11-19 16:05:44 +0300

    create main container

--- reactPizza/client/src/components/Sort.jsx
+++ reactPizza/client/src/components/Sort.jsx
@@ -1,4 +1,11 @@
+import React from "react";
+
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+
+  const list = ['попудярности', 'цене', 'алфабиту']
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -12,18 +19,26 @@ function Sort() {
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
-          ></path>
+          />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={()=>setOpen(!open)}>популярности</span>
       </div>
-      {/* <div className="sort_popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цунв</li>
-          <li>алфабиту</li>
-        </ul>
-      </div> */}
+      {open && (
+        <div className="sort__popup">
+          <ul>
+          {list.map((name, i) => (
+            <li
+              key ={i}
+              onClick={() => setSelected(i)}
+              className={selected === i ? "active" : ""}
+            >
+              {name}
+            </li>
+          ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
